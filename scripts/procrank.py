c8b00e1ead6665545a0dad4e80029e2c32b235a3
ouguangqian <ou.guangqian@hotmail.com>
2016-09-03 11:24:05 +0800

    script modfied

--- scripts/procrank.py
+++ scripts/procrank.py
@@ -4,29 +4,38 @@ import os
 import sys
 
 # file = open('procranktest.log', 'a')
-# file.writelines(' Vss      Rss      Pss      Uss \n')
-print(' Vss      Rss      Pss      Uss  \n')
+# file.writelines(' Vss      Rss      Pss      Uss    uss_max  \n')
+print(' Vss      Rss      Pss      Uss    uss_max  \n')
 # file.close()
-
-for i in range(100):
-    ret = os.popen('adb shell procrank | grep com.autonavi.amapauto').readlines()
+total_uss = 0
+uss_max = 0
+loop_cnt = 20
+for i in range(loop_cnt):
+    ret = os.popen('adb shell procrank | grep com.pateo.foton.icall').readlines()
     vss = 0
     rss = 0
     pss = 0
     uss = 0
+
+
     for r in ret:
         if len(r) > 0:
             while r.__contains__('  '):
                 r = r.replace('  ', ' ')
-            retlist = r.split(' ')
+            retlist = r.strip().split(' ')
             # print(retlist)
-            vss += int(retlist[2][:-1])
-            rss += int(retlist[3][:-1])
-            pss += int(retlist[4][:-1])
-            uss += int(retlist[5][:-1])
+            vss += int(retlist[1][:-1])
+            rss += int(retlist[2][:-1])
+            pss += int(retlist[3][:-1])
+            uss += int(retlist[4][:-1])
+    uss_max = max(uss, uss_max)
+    total_uss += uss
 
-    retStr = str(vss) + '   ' + str(rss) + '   ' + str(pss) + '   ' + str(uss)
+    retStr = str(vss) + '   ' + str(rss) + '   ' + str(pss) + '   ' + str(uss) + '   ' + str(uss_max)
     print(retStr)
+
+print('平均占用内存为：' + str(total_uss / loop_cnt ) + 'kb')
+print('最大内存为：' + str(uss_max) + 'kb')
             # file = open('/home/ogq/procranktest.log', 'a')
             # file.writelines(r)
             # file.close()
